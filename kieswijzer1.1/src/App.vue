bc8851b66b94b75e492348747b8ac2f140349838
marvinw2000 <Anthonie2000@outlook.com>
2021-09-06 09:52:10 +0200

    eerstenCommitHeaderGemaakt

---
+++ kieswijzer1.1/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+    <Header/>
+
+  </div>
+</template>
+<script>
+import Header from "./components/Header";
+export default {
+  name: 'App',
+  components: {
+    Header
+  }
+}
+</script>
+<style>
+*{
+  padding: 0px;
+  margin: 0px;
+}
+</style>
---
+++ kieswijzer1.1/src/components/Header.vue
@@ -0,0 +1,79 @@
+
+<template>
+  <header>
+    <div id="upper">
+      <div id="logo">
+        <img  v-bind:src="logoHeader" :alt="logoHeader"/>
+      </div>
+      <div id="titel">
+        <h1>{{ title }}</h1>
+      </div>
+    </div>
+    <div id="lower">
+      <div id="nav">
+        <a class="button" href="">Ict</a>
+        <a class="button" href="">A&M</a>
+        <a class="button" href="">T&E</a>
+        <a class="button" href="">MEI</a>
+        <a class="button" href="">B&I</a>
+        <a class="button" href="">kieswijzer</a>
+      </div>
+    </div>
+  </header>
+</template>
+<script>
+export default
+{
+  name: "Header",
+  data()
+  {
+    return{
+      title:"roc mondriaan kieswijzer",
+      logoHeader: require('@/assets/logo.jpeg')
+    }
+  },
+  methods: {
+
+  }
+}
+</script>
+<style scoped>
+#titel{}
+#titel h1{margin: 20px;}
+#logo{}
+#logo img{ height: 50px; width: 50px; margin: 20px;}
+#upper
+{
+  width: 100%;
+  height: 90px;
+  border-bottom: solid #333 1px;
+  display: flex;
+}
+#lower
+{
+  width: 100%;
+  height: 47px;
+  border-bottom: solid #333 1px;
+  display: flex;
+}
+#nav
+{
+  margin: 15px;
+}
+.button
+{
+  padding: 15px;
+  text-decoration: none;
+  border-right: solid #333 1px;
+  color: black;
+}
+#zoekBar
+{
+  float: right;
+  margin: 15px;
+}
+#test
+{
+  margin: 15px;
+}
+</style>
