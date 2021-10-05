de84c9ba39e54583ae67f261e5a3c80ef86cd844
M Sarikaya <31892563+16090209@users.noreply.github.com>
2021-10-05 13:38:18 +0200

    Header aangepast

--- kieswijzer1.1/src/components/Header.vue
+++ kieswijzer1.1/src/components/Header.vue
@@ -1,9 +1,12 @@
 
 <template>
   <header>
-    <div id="upper">
+    <div class="upper">
       <div id="logo">
         <img  v-bind:src="logoHeader" :alt="logoHeader"/>
+      </div>
+      <div class="buttonOpleidingen">
+        <button type="button">opleidingen</button>
       </div>
     </div>
   </header>
@@ -25,8 +28,34 @@ export default
 </script>
 <style scoped>
 #logo{}
-#logo img{ height: 50px; width: 50px; margin: 20px;}
+#logo img{
+  height: 50px;
+  width: 50px;
+  margin: 20px;
+}
 
+.upper{
+  width: 100%;
+  border-bottom: solid #999 1px;
+  display: flex;
+}
+
+.buttonOpleidingen{
+  margin-left: auto;
+}
+
+button{
+
+  background-color: #E20D18;
+  color:white;
+  font-size: 15px;
+  padding: 15px 20px 15px 20px;
+  margin-top: 20px;
+  margin-right: 35px;
+  border: none;
+  border-radius: 20px;
+
+}
 
 
 </style>
