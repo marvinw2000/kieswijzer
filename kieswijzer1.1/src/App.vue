f22ace644bf2ccc2a159fee29bf70d5ae8cbdd60
M Sarikaya <31892563+16090209@users.noreply.github.com>
2021-10-05 13:01:56 +0200

    wijzigingen homepagina

--- kieswijzer1.1/src/App.vue
+++ kieswijzer1.1/src/App.vue
@@ -1,17 +1,20 @@
 <template>
   <div id="app">
     <Header/>
+    <Home/>
     <div id="placeholder"></div>
     <Footer/>
   </div>
 </template>
 <script>
 import Header from "../src/components/Header";
+import Home from "../src/components/Home"
 import Footer from "../src/components/Footer";
 export default {
   name: 'App',
   components: {
     Header,
+    Home,
     Footer
   }
 }
--- kieswijzer1.1/src/components/Home.vue
+++ kieswijzer1.1/src/components/Home.vue
@@ -1,5 +1,14 @@
 <template>
-home
+  <body>
+    <div class="homeContainer">
+     <h1 class="homeHeaderText">Heb jij al een keuze gemaakt?</h1>
+      <p>
+        orem ipsum dolor sit amet, consectetur adipiscing elit.<br>
+        Aenean vitae ullamcorper neque
+      </p>
+     <button class="startButton" type="button">Start kieswijzer!</button>
+    </div>
+  </body>
 </template>
 
 <script>
@@ -9,5 +18,41 @@ export default {
 </script>
 
 <style scoped>
+
+body{
+  background-color: #1A61A1;
+  height: auto !important;
+  min-height: 100%;
+  width: 100%;
+}
+.homeContainer{
+  width: 100%;
+  text-align: center;
+  color:white;
+
+}
+
+.homeHeaderText{
+  padding-top: 50px;
+  margin-bottom: 30px;
+}
+
+.startButton{
+  background-color: #E20D18;
+  color: white;
+  font-size: 25px;
+  margin-top: 50px;
+  padding: 35px 40px 35px 40px;
+  border-radius: 35px;
+  margin-bottom: 50px;
+  border: none;
+}
+
+.startButton:hover{
+  padding: 40px 45px 40px 45px;
+  background-color: #E20D33;
+
+}
+
 
 </style>
