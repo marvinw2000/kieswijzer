918e8962dabddea97ee2f3d7a614eb3864fe5eb7
marvinw2000 <Anthonie2000@outlook.com>
2022-01-12 13:25:12 +0100

    updateGefixt

--- kieswijzer1.1/src/components/Crud.vue
+++ kieswijzer1.1/src/components/Crud.vue
@@ -6,7 +6,9 @@
           <div class="row">
             <div class="col-sm-10"><h2>Beheerders <b>dashboard</b></h2></div>
             <router-link class="col-sm-2" to="nieuw" exact>
-             <div ><button class="buttonToevoegen">Vraag toevoegen</button></div>
+              <div>
+                <button class="buttonToevoegen">Vraag toevoegen</button>
+              </div>
             </router-link>
           </div>
         </div>
@@ -25,19 +27,35 @@
           </thead>
           <tbody>
           <tr v-for="(vraag, index) in vragenData" :key="index">
-            <td>{{ vraag.id }}</td>
-            <td><div contenteditable :class="[vraag.id]" >{{ vraag.vraag }}</div></td>
-            <td><div contenteditable >{{ vraag.juisteAntwoord }}</div></td>
-            <td><div contenteditable >{{ vraag.puntenIct }}</div></td>
-            <td><div contenteditable >{{ vraag.puntenAenM }}</div></td>
-            <td><div contenteditable >{{ vraag.puntenBenI }}</div></td>
-            <td><div contenteditable >{{ vraag.puntenMei }}</div></td>
-            <td><div contenteditable >{{ vraag.puntenTenI }}</div></td>
+            <td>{{ index + 1 }}</td>
             <td>
-<!--          <router-link to="update">-->
-              <a v-on:click="updateAction(vraag.id)" class="edit" title="Edit" data-toggle="tooltip"><i class="material-icons">&#xE254;</i></a>
-<!--          </router-link>-->
-              <a href="#" v-on:click="deleteVraag(vraag.id)" class="delete" title="Delete" data-toggle="tooltip"><i class="material-icons ">&#xE872;</i></a>
+              <div contenteditable class="vraagNaam">{{ vraag.vraag }}</div>
+            </td>
+            <td>
+              <div contenteditable class="juisteAntwoord">{{ vraag.juisteAntwoord }}</div>
+            </td>
+            <td>
+              <div contenteditable class="puntenIct">{{ vraag.puntenIct }}</div>
+            </td>
+            <td>
+              <div contenteditable class="puntenAenM">{{ vraag.puntenAenM }}</div>
+            </td>
+            <td>
+              <div contenteditable class="puntenBenI">{{ vraag.puntenBenI }}</div>
+            </td>
+            <td>
+              <div contenteditable class="puntenMei">{{ vraag.puntenMei }}</div>
+            </td>
+            <td>
+              <div contenteditable class="puntenTenI">{{ vraag.puntenTenI }}</div>
+            </td>
+            <td>
+              <!--          <router-link to="update">-->
+              <a v-on:click="updateAction(index, vraag.id)" class="edit" title="Edit" data-toggle="tooltip"><i
+                  class="material-icons">&#xE254;</i></a>
+              <!--          </router-link>-->
+              <a href="#" v-on:click="deleteVraag(vraag.id)" class="delete" title="Delete" data-toggle="tooltip"><i
+                  class="material-icons ">&#xE872;</i></a>
             </td>
           </tr>
           </tbody>
@@ -63,27 +81,61 @@ export default {
           //console.log(this.vragenData[0].vraag)
         });
   },
-  data(){
-    return{
-      vragenData: null,
-
+  data() {
+    return {
+      vragenData: null
 
     }
   },
-  methods:{
-    deleteVraag(id){
-      if (confirm(`weet je zeker dat je vraag ${id} wilt verwijderen`)){
+  methods: {
+    deleteVraag(id) {
+      if (confirm(`weet je zeker dat je vraag ${id} wilt verwijderen`)) {
         fetch(`https://127.0.0.1:8000/deleteQuestion/${id}`)
         this.$router.go('crud')
       }
     },
-    updateAction(id){
-      //let vraagNaam1 = document.getElementsByClassName(`vraagNaam ${id}`)
-      let vraagNaam2 = document.querySelectorAll(id)
-      // console.log(vraagNaam1.textcontent)
-      console.log(vraagNaam2.textcontent)
-      //fetch(`https://127.0.0.1:8000/updateQuestion/${id}`)
-      //this.$router.go('crud')
+    updateAction(updateId, id) {
+      let allQuestions = document.getElementsByClassName('vraagNaam')
+      let currentQuestion = allQuestions[updateId]
+
+      let allCorrectAnswer = document.getElementsByClassName('juisteAntwoord')
+      let currentCorectAnswer = allCorrectAnswer[updateId]
+
+      let allPointsIct = document.getElementsByClassName('puntenIct')
+      let currentPointsIct = allPointsIct[updateId]
+
+      let allPointsAenM = document.getElementsByClassName('puntenAenM')
+      let currentPointsAenM = allPointsAenM[updateId]
+
+      let allPointsBenI = document.getElementsByClassName('puntenBenI')
+      let currentPointsBenI = allPointsBenI[updateId]
+
+      let allPointsMei = document.getElementsByClassName('puntenMei')
+      let currentPointsMei = allPointsMei[updateId]
+
+      let allPointsTenI = document.getElementsByClassName('puntenTenI')
+      let currentPointsTenI = allPointsTenI[updateId]
+
+      const json = {
+        "currentQuestion": currentQuestion.textContent,
+        "currentCorectAnswer": currentCorectAnswer,
+        "currentPointsIct": currentPointsIct.textContent,
+        "currentPointsAenM": currentPointsAenM.textContent,
+        "currentPointsBenI": currentPointsBenI.textContent,
+        "currentPointsMei": currentPointsMei.textContent,
+        "currentPointsTenI": currentPointsTenI.textContent
+      }
+      fetch(`https://127.0.0.1:8000/updateQuestion/${id}`, {
+        body: JSON.stringify(json),
+        method: "POST"
+      }).then((response) =>{
+        console.log("test")
+        return response.json();
+          })
+      .then((myJson)=>{
+        console.log(myJson);
+      })
+      this.$router.go('crud')
     }
   }
 }
@@ -98,24 +150,31 @@ body {
 .table-responsive {
   margin: 30px 0;
 }
+
 .table-wrapper {
   min-width: 1000px;
   background: #fff;
   padding: 20px;
-  box-shadow: 0 1px 1px rgba(0,0,0,.05);
+  box-shadow: 0 1px 1px rgba(0, 0, 0, .05);
+}
+.edit{
+  cursor: pointer;
 }
 .table-title {
   padding-bottom: 10px;
   margin: 0 0 10px;
 }
+
 .table-title h2 {
   margin: 8px 0 0;
   font-size: 22px;
 }
+
 .search-box {
   position: relative;
   float: right;
 }
+
 .search-box input {
   height: 34px;
   border-radius: 20px;
@@ -123,9 +182,11 @@ body {
   border-color: #ddd;
   box-shadow: none;
 }
+
 .search-box input:focus {
   border-color: #3FBAE4;
 }
+
 .search-box i {
   color: #a0a5b1;
   position: absolute;
@@ -133,23 +194,29 @@ body {
   top: 8px;
   left: 10px;
 }
+
 table.table tr th, table.table tr td {
   border-color: #e9e9e9;
 }
+
 table.table-striped tbody tr:nth-of-type(odd) {
   background-color: #fcfcfc;
 }
+
 table.table-striped.table-hover tbody tr:hover {
   background: #f5f5f5;
 }
+
 table.table th i {
   font-size: 13px;
   margin: 0 5px;
   cursor: pointer;
 }
+
 table.table td:last-child {
   width: 130px;
 }
+
 table.table td a {
   color: #a0a5b1;
   display: inline-block;
@@ -159,16 +226,20 @@ table.table td a {
 table.table td a.edit {
   color: #FFC107;
 }
+
 table.table td a.delete {
   color: red;
 }
+
 table.table td i {
   font-size: 19px;
 }
+
 .pagination {
   float: right;
   margin: 0 0 5px;
 }
+
 .pagination li a {
   border: none;
   font-size: 95%;
@@ -181,25 +252,31 @@ table.table td i {
   text-align: center;
   padding: 0;
 }
+
 .pagination li a:hover {
   color: #666;
 }
+
 .pagination li.active a {
   background: #03A9F4;
 }
+
 .pagination li.active a:hover {
   background: #0397d6;
 }
+
 .pagination li.disabled i {
   color: #ccc;
 }
+
 .pagination li i {
   font-size: 16px;
   padding-top: 6px
 }
-.buttonToevoegen{
+
+.buttonToevoegen {
   background-color: #E30513;
-  color:white;
+  color: white;
   font-size: 15px;
   padding: 15px 20px 15px 20px;
   border: none;
